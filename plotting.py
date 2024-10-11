masking 2.0 + fix db locked bug

plotting.py
@@ -1,7 +1,9 @@
+import os
+import subprocess
+
+import matplotlib.pyplot as plt
 import numpy as np
 from astropy.visualization import simple_norm
-import matplotlib.pyplot as plt
-import os
 
 
 def plot_cutout(cutout, in_dict, figure_dir, show_plot=True, save_plot=False):
@@ -18,7 +20,7 @@ def plot_cutout(cutout, in_dict, figure_dir, show_plot=True, save_plot=False):
     tile = cutout['tile']
     obj_ids = np.array([x.decode('utf-8') for x in cutout['cfis_id']])
     n_objects, n_bands = image_data.shape[0], image_data.shape[1]
-    fig, axes = plt.subplots(n_objects, n_bands, figsize=(n_bands*4, n_objects*4))
+    fig, axes = plt.subplots(n_objects, n_bands, figsize=(n_bands * 4, n_objects * 4))
 
     # Make sure axes is always a 2D array
     if n_objects == 1:
@@ -33,7 +35,7 @@ def plot_cutout(cutout, in_dict, figure_dir, show_plot=True, save_plot=False):
             # Get the image data for the current object and filter band
             image = image_data[i, j]
             # Display the image
-            norm = simple_norm(image, 'sqrt', percent=98.)
+            norm = simple_norm(image, 'sqrt', percent=98.0)
             ax.imshow(image, norm=norm, cmap='gray_r', origin='lower')  # Adjust the cmap as needed
             ax.set_title(f'{obj_ids[i]}, {filter_name}')
 
@@ -43,8 +45,41 @@ def plot_cutout(cutout, in_dict, figure_dir, show_plot=True, save_plot=False):
     plt.tight_layout()
 
     if save_plot:
-        plt.savefig(os.path.join(figure_dir, f'cutouts_tile_{tile[0]}_{tile[1]}.pdf'), bbox_inches='tight', dpi=300)
+        plt.savefig(
+            os.path.join(figure_dir, f'cutouts_tile_{tile[0]}_{tile[1]}.pdf'),
+            bbox_inches='tight',
+            dpi=300,
+        )
     if show_plot:
         plt.show()
     else:
         plt.close()
+
+
+def open_fits_files_in_ds9(file1_path, file2_path):
+    try:
+        # Construct the DS9 command with all the required options
+        cmd = [
+            'ds9',
+            '-single',
+            '-frame 1',
+            file1_path,
+            '-scale zscale',
+            '-frame new',
+            file2_path,
+            '-scale zscale',
+            '-frame lock wcs',
+            '-wcs sky icrs',  # Set WCS to ICRS
+            '-wcs skyformat degrees',  # Display coordinates in degrees
+            '-geometry 1920x1080+0+0',
+        ]
+
+        # Run the DS9 command
+        subprocess.run(cmd, check=True)
+        print(
+            f'Successfully opened {os.path.basename(file1_path)} and {os.path.basename(file2_path)} in DS9.'
+        )
+    except subprocess.CalledProcessError:
+        print('Error: Failed to open the FITS files in DS9.')
+    except FileNotFoundError:
+        print('Error: DS9 is not installed or not in your system PATH.')

postprocess.py
@@ -1,6 +1,7 @@
 import os
 import time
 
+import cv2
 import h5py
 import numpy as np
 import pandas as pd
@@ -139,31 +140,72 @@ def match_cats_older(df_det, df_label, tile, max_sep=15.0):
     return det_matching_idx, label_matches, label_unmatches, det_matches
 
 
-def match_stars(df_det, df_label, max_sep=2.0):
+def match_stars(
+    df_det, df_label, segmap=None, max_sep=5.0, extended_flag_radius=None, mag_limit=14.0
+):
     """
-    Match detections to known objects, return matches, unmatches
-
+    Match detections to known objects, return matches, unmatches, and extended flag indices
     Args:
         df_det (dataframe): detections dataframe
         df_label (dataframe): dataframe of objects with labels
-        max_sep (float): maximum separation tollerance
-
+        max_sep (float): maximum separation tolerance for direct matches
+        extended_flag_radius (float): radius to flag detections as potentially associated with stars
     Returns:
         det_matching_idx (list): indices of detections for which labels are available
         label_matches (dataframe): known objects that were detected
+        label_unmatches (dataframe): known objects that were not detected
         det_matches (dataframe): detections that are known objects
+        extended_flag_idx (list): indices of all detections within extended_flag_radius of known stars
     """
-    c_det = SkyCoord(df_det['ra'], df_det['dec'], unit=u.deg)  # type: ignore
-    c_label = SkyCoord(df_label['ra'], df_label['dec'], unit=u.deg)  # type: ignore
+    c_det = SkyCoord(df_det['ra'], df_det['dec'], unit=u.deg)
+    c_label = SkyCoord(df_label['ra'], df_label['dec'], unit=u.deg)
 
+    # Find closest matches within max_sep
     idx, d2d, _ = c_label.match_to_catalog_3d(c_det)
-    # sep_constraint is a list of True/False
-    sep_constraint = d2d < max_sep * u.arcsec  # type: ignore
+    sep_constraint = d2d < max_sep * u.arcsec
+    det_matching_idx = idx[sep_constraint]
+
     label_matches = df_label[sep_constraint].reset_index(drop=True)
     label_unmatches = df_label[~sep_constraint].reset_index(drop=True)
-    det_matching_idx = idx[sep_constraint]  # det_matching_idx is a list of indices
     det_matches = df_det.loc[det_matching_idx].reset_index(drop=True)
-    return det_matching_idx, label_matches, label_unmatches, det_matches
+
+    extended_flag_idx, extended_flag_mags = None, None
+    if extended_flag_radius is not None:
+        bright_stars = df_label[df_label['Gmag'] < mag_limit].reset_index(drop=True)
+
+        # Create a mask for all bright stars at once using cv2.circle
+        h, w = segmap.shape
+        all_stars_mask = np.zeros((h, w), dtype=np.uint8)
+        for x, y in bright_stars[['x', 'y']].values:
+            x, y = round(x), round(y)
+            if 0 <= x < w and 0 <= y < h:
+                cv2.circle(all_stars_mask, (x, y), round(extended_flag_radius), 1, -1)
+
+        # Find all segment IDs within the masked area
+        segment_ids = np.unique(segmap[all_stars_mask > 0])[1:]  # exclude 0 (background)
+
+        extended_flag_idx = (
+            segment_ids - 1
+        )  # Subtract 1 because SEP IDs start at 1, but DataFrame index starts at 0
+
+        # Find the closest bright star for each flagged segment
+        flagged_segments_centers = df_det.loc[extended_flag_idx, ['xpeak', 'ypeak']].values
+        bright_stars_coords = bright_stars[['x', 'y']].values
+        distances = np.sqrt(
+            np.sum((flagged_segments_centers[:, np.newaxis] - bright_stars_coords) ** 2, axis=2)
+        )
+        closest_star_indices = np.argmin(distances, axis=1)
+
+        extended_flag_mags = bright_stars.loc[closest_star_indices, 'Gmag'].values
+
+    return (
+        det_matching_idx,
+        label_matches,
+        label_unmatches,
+        det_matches,
+        extended_flag_idx,
+        extended_flag_mags,
+    )
 
 
 def match_cats(df_det, df_label, tile, pixel_scale, max_sep=15.0, re_multiplier=3.0):
@@ -539,7 +581,9 @@ def add_redshifts(det_df, z_class_cat):
 
     if len(class_z_df) > 0:
         # match detections to redshift and class catalog
-        det_idx_z_class, label_matches_z_class, _, _ = match_stars(det_df, class_z_df, max_sep=5.0)
+        det_idx_z_class, label_matches_z_class, _, _, _, _ = match_stars(
+            det_df, class_z_df, max_sep=5.0
+        )
         det_idx_z_class = det_idx_z_class.astype(np.int32)  # make sure index is int
 
         if len(det_idx_z_class) > 0:

tile_cutter.py
@@ -10,7 +10,7 @@ from astropy.wcs.utils import skycoord_to_pixel
 
 from kd_tree import TileWCS, query_tree, relate_coord_tile
 from logging_setup import get_logger
-from utils import tile_str
+from utils import decompress_fits, tile_str
 
 logger = get_logger()
 
@@ -131,6 +131,9 @@ def download_tile_one_band(tile_numbers, tile_fitsname, final_path, temp_path, v
         logger.info(
             f'Successfully downloaded tile {tile_str(tile_numbers)} for band {band} in {np.round(time.time()-start_time, 1)} seconds.'
         )
+        # HSC data is compressed, decompress for faster read speed
+        if band in ['whigs-g', 'wishes-z']:
+            decompress_fits(final_path)
         return True
 
     except subprocess.CalledProcessError as e:

utils.py
@@ -16,6 +16,7 @@ from astropy.io import fits
 from astropy.stats import SigmaClip
 from astropy.wcs import WCS
 from astroquery.gaia import Gaia
+from numpy import linalg as LA
 from photutils.background import Background2D
 from scipy.spatial import cKDTree
 from scipy.stats import truncnorm
@@ -60,13 +61,15 @@ def func_PCA(x, y):
     return axis_ratio, minor_axis, pca
 
 
-def query_gaia_stars(target_coord, r_arcsec):
+def query_gaia_stars(target_coord, r_arcsec, max_retries=3, retry_delay=5):
     """
     Query Gaia DR3 for non-galaxy sources within the defined search radius.
 
     Args:
         target_coord (numpy.2darray): coordinates of the tile center.
         r_arcsec (float): search radius in arcseconds.
+        max_retries (int): maximum number of retires after a failed attempt.
+        retry_delay (int): seconds of delay between retries.
 
     Returns:
         table (dataframe): non-galaxy Gaia sources within the search radius.
@@ -74,22 +77,33 @@ def query_gaia_stars(target_coord, r_arcsec):
     Gaia.MAIN_GAIA_TABLE = 'gaiadr3.gaia_source'  # type: ignore # Select Data Release 3
     Gaia.ROW_LIMIT = -1  # unlimited rows
     columns = ['source_id', 'ra', 'dec', 'phot_g_mean_mag', 'in_galaxy_candidates']
-    # Query Gaia DR3 for sources within the defined search radius
-    job = Gaia.cone_search_async(
-        target_coord, radius=u.Quantity(r_arcsec, u.arcsec), columns=columns
-    )
-    table = job.get_results()
-    # Check if the results come back empty, if so return an empty dataframe
-    if table is None:
-        return pd.DataFrame()
-    else:
-        table = table.to_pandas()
-    # Remove sources that are galaxy candidates
-    table = table.loc[~table['in_galaxy_candidates']].reset_index(drop=True)
-    table.rename(columns={'phot_g_mean_mag': 'Gmag'}, inplace=True)
-    # Remove sources with Gmag >= 20.6
-    table = table.loc[table['Gmag'] < 20.6].reset_index(drop=True)
-    return table
+
+    for attempt in range(max_retries):
+        try:
+            # Query Gaia DR3 for sources within the defined search radius
+            job = Gaia.cone_search_async(
+                target_coord, radius=u.Quantity(r_arcsec, u.arcsec), columns=columns
+            )
+            table = job.get_results()
+
+            # Check if the results come back empty, if so return an empty dataframe
+            if table is None:
+                return pd.DataFrame()
+            else:
+                table = table.to_pandas()
+            # Remove sources that are galaxy candidates
+            table = table.loc[~table['in_galaxy_candidates']].reset_index(drop=True)
+            table.rename(columns={'phot_g_mean_mag': 'Gmag'}, inplace=True)
+            # Remove sources with Gmag >= 20.6
+            table = table.loc[table['Gmag'] < 20.6].reset_index(drop=True)
+            return table
+        except Exception as e:
+            if attempt < max_retries - 1:
+                logger.error(f'Error occurred: {str(e)}. Retrying in {retry_delay} seconds...')
+                time.sleep(retry_delay)
+            else:
+                logger.error(f'Max retries reached. Last error: {str(e)}')
+                return pd.DataFrame()
 
 
 def remove_background(image):
@@ -493,10 +507,16 @@ def check_objects_in_neighboring_tiles(tile, dwarfs_df, header):
 
 
 def get_dwarf_tile_list(dwarf_cat, in_dict, bands=None):
-    bands = [in_dict[band]['band'] for band in bands]
-    dwarf_cat_filtered = get_df_for_bands(dwarf_cat, bands)
-    dwarf_tiles_for_bands = dwarf_cat_filtered['tile'].values
-    str_to_tuple = [ast.literal_eval(item) for item in dwarf_tiles_for_bands]
+    try:
+        bands = [in_dict[band]['band'] for band in bands]
+        dwarf_cat_filtered = get_df_for_bands(dwarf_cat, bands)
+        dwarf_tiles_for_bands = dwarf_cat_filtered['tile'].values
+    except Exception as e:
+        print(f'Error getting known dwarf tiles in r: {e}')
+    try:
+        str_to_tuple = [ast.literal_eval(item) for item in dwarf_tiles_for_bands]
+    except Exception as e:
+        print(f'Error in str_to_tuple: {e}')
     unique_tiles = set(str_to_tuple)
     return unique_tiles
 
@@ -517,7 +537,10 @@ def check_bands(bands_str, to_check):
 
 
 def get_df_for_bands(dwarf_cat, check_for_bands):
-    dwarf_cat_df = pd.read_csv(dwarf_cat)
+    if isinstance(dwarf_cat, str):
+        dwarf_cat_df = pd.read_csv(dwarf_cat)
+    else:
+        dwarf_cat_df = dwarf_cat
     df_select = dwarf_cat_df.loc[
         (~dwarf_cat_df['tile'].isna())
         & (dwarf_cat_df['bands'].apply(lambda x: check_bands(x, check_for_bands)))
@@ -694,3 +717,54 @@ def transform_path(path):
         return f'/{parts[0]}:{parts[1]}'
     else:
         return path  # Return the original path if there's no slash to replace
+
+
+def dist_peak_center(obj):
+    x, y = round(obj['xpeak'].iloc[0]), round(obj['ypeak'].iloc[0])
+    xmin, xmax = round(obj['xmin'].iloc[0]), round(obj['xmax'].iloc[0])
+    ymin, ymax = round(obj['ymin'].iloc[0]), round(obj['ymax'].iloc[0])
+    x_center, y_center = np.mean([xmin, xmax]), np.mean([ymin, ymax])
+    npix = obj['npix'].iloc[0]
+    # Get euclidean distance between the photometric center and the center of the segment
+    d_peak_center = LA.norm(np.array([x_center, y_center]) - np.array([x, y]))
+    # normalized by area
+    d_norm = d_peak_center / npix
+
+    return d_peak_center, d_norm
+
+
+def estimate_axis_ratio(labeled_array, label):
+    y, x = np.nonzero(labeled_array == label)
+    if len(y) < 2:  # Handle single-pixel objects
+        return 1.0
+    y = y.astype(np.float32)
+    x = x.astype(np.float32)
+    y -= y.mean()
+    x -= x.mean()
+    inertia = np.array([[np.sum(y**2), np.sum(x * y)], [np.sum(x * y), np.sum(x**2)]])
+    eigenvalues = np.linalg.eigvals(inertia)
+    minor, major = np.sqrt(np.abs(eigenvalues))
+    return minor / major if major != 0 else 1.0
+
+
+def ensure_list(variable):
+    if isinstance(variable, str):
+        return [variable]
+    elif isinstance(variable, list):
+        return variable
+    else:
+        raise TypeError('Input should be either a string or a list.')
+
+
+def decompress_fits(file_path, fits_ext=1):
+    """
+    Decompress fits file by reading and saving again.
+
+    Args:
+        file_path (str): path to file
+        fits_ext (int, optional): data extension. Defaults to 1.
+    """
+    data, header = open_fits(file_path, fits_ext=fits_ext)
+    new_hdu = fits.PrimaryHDU(data=data.astype(np.float32), header=header)
+    # save new fits file
+    new_hdu.writeto(file_path, overwrite=True)